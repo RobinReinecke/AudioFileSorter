41943c83b757eebdbe3d849e3bef56cfe1aab7af
Robin Reinecke <robinreinecke@live.de>
2018-01-09 08:38:45 +0100

    some bugfixes

--- AudioFileSorter/Program.cs
+++ AudioFileSorter/Program.cs
@@ -63,6 +63,7 @@ namespace AudioFileSorter
             var output_moves = false;
             var show_help = false;
             var custom_formats = new List<string>();
+
             //ndesk parameters
             var parameters = new OptionSet()
             {
@@ -103,9 +104,9 @@ namespace AudioFileSorter
                 if (destination_dir == null)
                     throw new OptionException("Missing parameter", destination_dir);
                 if (!Directory.Exists(destination_dir))
-                    throw new OptionException("Directory does not exists.", destination_dir);
+                    throw new OptionException("Directory does not exist.", destination_dir);
                 if (!Directory.Exists(source_dir))
-                    throw new OptionException("Directory does not exists.", source_dir);
+                    throw new OptionException("Directory does not exist.", source_dir);
             }
             catch (OptionException ex)
             {
@@ -113,75 +114,79 @@ namespace AudioFileSorter
                 return;
             }
 
+            //Add custom media formats
+            media_formats.AddRange(custom_formats);
 
             CreateDirStructur(destination_dir);
-            ProcessDirectory(source_dir, destination_dir, output_moves, custom_formats);
+            ProcessDirectory(source_dir, destination_dir, output_moves);
 
             Console.WriteLine("\n\nFinished");
         }
 
+        /// <summary>
+        /// Create the directory structur for sorting the files to.
+        /// </summary>
         private static void CreateDirStructur(string destination_Dir)
         {
             Directory.CreateDirectory(destination_Dir + "\\0-9");
+            Directory.CreateDirectory(destination_Dir + "\\_unsortable");
+
             foreach (var c in alphabet)
-            {
                 Directory.CreateDirectory(destination_Dir + "\\" + c);
-            }
         }
 
-        private static void ProcessDirectory(string source_Dir, string destination_Dir, bool output_Moves, List<string> custom_formats)
+        /// <summary>
+        /// Main method to sort all the files.
+        /// </summary>
+        private static void ProcessDirectory(string source_Dir, string destination_Dir, bool output_Moves)
         {
-            media_formats.AddRange(custom_formats);
-
             foreach (var file in GetFiles(source_Dir, media_formats, SearchOption.AllDirectories))
             {
                 //get media information
                 var tagFile = TagLib.File.Create(file);
-                var album_artists = tagFile.Tag.AlbumArtists;
+                var performers = tagFile.Tag.Performers;
                 var album = tagFile.Tag.Album;
                 var title = tagFile.Tag.Title;
 
-                if (album_artists == null || album == null || title == null)
+                if (performers == null || album == null || title == null)
                 {
                     Console.WriteLine("Can't read enough information to sort " + file);
                     continue;
                 }
 
-                var primaryArtist = album_artists[0];
+                var primaryPerformer = performers[0];
 
-                //Case of multiple album artists. Don't know if its possible
-                if (album_artists.Length > 1)
+                //Case of multiple performers. Don't know if its possible
+                if (performers.Length > 1)
                 {
-                    var output = "Multiple album artists found. Which is the primary artist?";
-                    for (var i = 1; i <= output.Length; i++)
-                        output += i + ". " + album_artists[i - 1];
+                    var output = "Multiple performers found. Which is the primary performer? ";
+                    for (var i = 1; i <= performers.Length; i++)
+                        output += i + ". " + performers[i - 1] + " ";
                     Console.WriteLine(output);
                     var selectedIndex = -1;
 
-                    while (selectedIndex > album_artists.Length || selectedIndex < 1)
+                    while (selectedIndex > performers.Length || selectedIndex < 1)
                     {
                         Console.Write("Index: ");
                         int.TryParse(Console.ReadLine(), out selectedIndex);
                     }
 
-                    primaryArtist = album_artists[selectedIndex - 1];
-
+                    primaryPerformer = performers[selectedIndex - 1];
                 }
-
 
                 var destination = destination_Dir;
 
-                if (Regex.IsMatch(primaryArtist[0].ToString(), "[a-zöäü]", RegexOptions.IgnoreCase))
+                if (Regex.IsMatch(primaryPerformer[0].ToString(), "[a-zöäü]", RegexOptions.IgnoreCase))
                 {
-                    destination += "\\" + primaryArtist[0].ToString().ToUpper() + "\\" + primaryArtist;
+                    destination += "\\" + primaryPerformer[0].ToString().ToUpper() + "\\" + primaryPerformer;
                     Directory.CreateDirectory(destination);
                     destination += "\\" + album;
                     Directory.CreateDirectory(destination);
                     destination += "\\" + Path.GetFileName(file);
                 }  
-                else if (char.IsNumber(primaryArtist[0]))
+                else if (char.IsNumber(primaryPerformer[0]))
                 { 
-                    destination += "\\0-9\\" + primaryArtist;
+                    destination += "\\0-9\\" + primaryPerformer;
                     Directory.CreateDirectory(destination);
                     destination += "\\" + album;
                     Directory.CreateDirectory(destination);
@@ -189,17 +194,16 @@ namespace AudioFileSorter
                 }
                 else
                     destination += "\\_unsortable\\" + Path.GetFileName(file);
+
                 try
                 {
                     File.Move(file, destination);
                     if (output_Moves)
-                    {
                         Console.WriteLine("Moved " + file + " to " + destination);
-                    }
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Can't move file " + file);
+                    Console.WriteLine("Can't move file " + file + " to " + destination);
                 }
             }
         }
