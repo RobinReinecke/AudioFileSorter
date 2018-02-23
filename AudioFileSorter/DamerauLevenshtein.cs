2ef512d439007b2604cbec10078036e2ec5e9106
Robin Reinecke <robinreinecke@live.de>
2018-02-23 14:20:42 +0100

    fail

--- AudioFileSorter/DamerauLevenshtein.cs
+++ AudioFileSorter/DamerauLevenshtein.cs
@@ -1,7 +1,57 @@
-﻿namespace AudioFileSorter
+﻿using System;
+
+namespace AudioFileSorter
 {
-    public class DamerauLevenshtein
+    public static class DamerauLevenshtein
     {
-        
+        public static int DamerauLevenshteinDistanceTo(this string @string, string targetString)
+        {
+            return DamerauLevenshteinDistance(@string, targetString);
+        }
+
+        public static int DamerauLevenshteinDistance(string string1, string string2)
+        {
+            if (string.IsNullOrEmpty(string1))
+            {
+                return !string.IsNullOrEmpty(string2) ? string2.Length : 0;
+            }
+
+            if (string.IsNullOrEmpty(string2))
+            {
+                return !string.IsNullOrEmpty(string1) ? string1.Length : 0;
+            }
+
+            var length1 = string1.Length;
+            var length2 = string2.Length;
+
+            var d = new int[length1 + 1, length2 + 1];
+
+            int cost, del, ins, sub;
+
+            for (var i = 0; i <= d.GetUpperBound(0); i++)
+                d[i, 0] = i;
+
+            for (var i = 0; i <= d.GetUpperBound(1); i++)
+                d[0, i] = i;
+
+            for (var i = 1; i <= d.GetUpperBound(0); i++)
+            {
+                for (var j = 1; j <= d.GetUpperBound(1); j++)
+                {
+                    cost = string1[i - 1] == string2[j - 1] ? 0 : 1;
+
+                    del = d[i - 1, j] + 1;
+                    ins = d[i, j - 1] + 1;
+                    sub = d[i - 1, j - 1] + cost;
+
+                    d[i, j] = Math.Min(del, Math.Min(ins, sub));
+
+                    if (i > 1 && j > 1 && string1[i - 1] == string2[j - 2] && string1[i - 2] == string2[j - 1])
+                        d[i, j] = Math.Min(d[i, j], d[i - 2, j - 2] + cost);
+                }
+            }
+
+            return d[d.GetUpperBound(0), d.GetUpperBound(1)];
+        }
     }
 }
